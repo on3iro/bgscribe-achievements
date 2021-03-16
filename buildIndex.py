1c3fa14edb4be373088fde46797be13636e4a99d
Theo Salzmann <theo.salzmann@sandstorm.de>
2021-03-16 19:04:08 +0100

    TASK: Add indexing script

---
+++ buildIndex.py
@@ -0,0 +1,45 @@
+import os
+import json
+from algoliasearch.search_client import SearchClient
+
+def readAchievementFiles():
+    directory = r'./achievementSets'
+
+    fileContents = []
+
+    for entry in os.scandir(directory):
+        if (entry.path.endswith(".json") and entry.is_file()):
+            with open(entry.path, 'r') as file:
+                data = file.read()
+                fileContents.append({ 'data': data, 'name': file.name })
+
+    return fileContents
+
+def fileContentToIndexable(fileContent):
+    # parse file
+    name = os.path.splitext(os.path.basename(fileContent['name']))[0]
+    data = {
+        **json.loads(fileContent['data']),
+        'objectID': name
+    }
+
+    return data
+
+def mapFileContentsToIndexable(fileContents: list):
+    return map(fileContentToIndexable, fileContents)
+
+def main():
+    print('Indexing files...')
+
+    client = SearchClient.create('0AUXOSEYK3', os.environ['ADMIN_API_KEY'])
+    index = client.init_index('dev_BGScribe_Achievements')
+
+    indexableFileData = mapFileContentsToIndexable(readAchievementFiles())
+
+    index.replace_all_objects(indexableFileData)
+
+    print(f'Done.')
+
+
+if __name__ == "__main__":
+    main()
